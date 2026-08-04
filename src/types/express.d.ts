58b00b59fce887b37a2dd78aef1deb92171817ef
Rohit Bhojak <rohitbhojak99@gmail.com>
2026-08-04 23:31:08 +0530

    Override express default user with User type

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,7 +1,11 @@
+import { User as AppUser } from './database.ts';
 export {};
 
 declare global {
   namespace Express {
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+    interface User extends AppUser {}
+
     interface Response {
       renderView: (view: string, data?: Record<string, unknown>) => void;
     }
