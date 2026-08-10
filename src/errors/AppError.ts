0a9f749543d2975da0e1e59b5c64fe6d92161810
Rohit Bhojak <rohitbhojak99@gmail.com>
2026-08-10 22:57:55 +0530

    Add custom error class

---
+++ src/errors/AppError.ts
@@ -0,0 +1,22 @@
+export class AppError extends Error {
+  statusCode: number;
+  isOperational: boolean;
+  constructor(message: string, statusCode: number = 500) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+export class NotFoundError extends AppError {
+  constructor(message: string = 'Resource not found') {
+    super(message, 404);
+  }
+}
+
+export class UnauthorizedError extends AppError {
+  constructor(message: string = 'You must be logged in to access this resource') {
+    super(message, 401);
+  }
+}
